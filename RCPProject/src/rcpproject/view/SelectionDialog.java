64ce3de06ebd99e8e1bab08dfc5a1303626c8282
Igor Dojcinovic <igor@sonicline.rs>
2016-09-04 19:56:53 +0200

    pripremljena klasa za biranje izvornog fajla kroz dialog u jfaceu

---
+++ RCPProject/src/rcpproject/view/SelectionDialog.java
@@ -0,0 +1,27 @@
+package rcpproject.view;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+public class SelectionDialog extends Dialog {
+
+	protected SelectionDialog(Shell parentShell) {
+		super(parentShell);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void configureShell(Shell newShell) {
+		// TODO Auto-generated method stub
+		super.configureShell(newShell);
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// TODO Auto-generated method stub
+		return super.createDialogArea(parent);
+	}
+
+}
