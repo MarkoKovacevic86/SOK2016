ef14c069ccaaad7276fba050596cfdf85c1c620c
markokovacevic86 <markokovacevic93@hotmail.com>
2016-09-04 19:09:47 +0200

    Pripremeljena klasa za preuzimanje xml izvora za obradu

---
+++ RCPProject.xml.source/src/rcpproject/xml/source/XMLSource.java
@@ -0,0 +1,16 @@
+package rcpproject.xml.source;
+
+import org.eclipse.ui.IWorkbenchWindow;
+
+import rcpproject.model.Graph;
+import rcpproject.source.ISource;
+
+public class XMLSource implements ISource{
+
+	@Override
+	public Graph getSource(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
