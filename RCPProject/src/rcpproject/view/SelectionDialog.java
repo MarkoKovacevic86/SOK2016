5fcaba8220fa2be32ebfaf9b013c0f203822f2e3
Stefan <stefancina@live.com>
2016-09-06 16:49:54 +0200

    Pripremljen izgleda dialoga za odabir izvora kroz glavni prozor.

--- RCPProject/src/rcpproject/view/SelectionDialog.java
+++ RCPProject/src/rcpproject/view/SelectionDialog.java
@@ -1,12 +1,23 @@
 package rcpproject.view;
 
+import org.eclipse.draw2d.GridData;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+
+import rcpproject.source.SourceDescription;
+import rcpproject.source.SourceManager;
 
 public class SelectionDialog extends Dialog {
 
+	private Combo sourceCombos;
+	private String sourceName;
+	
 	protected SelectionDialog(Shell parentShell) {
 		super(parentShell);
 		// TODO Auto-generated constructor stub
@@ -21,7 +32,43 @@ public class SelectionDialog extends Dialog {
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		// TODO Auto-generated method stub
-		return super.createDialogArea(parent);
+		Composite c = new Composite(parent, SWT.BORDER);
+		GridLayout lay = new GridLayout(2, true);
+		c.setLayout(lay);
+		Label l = new Label(c, SWT.NONE);
+		l.setText("Source: ");
+		l.setLayoutData(new GridData(GridData.BEGINNING,GridData.CENTER,false,false));
+		sourceCombos = new Combo(c, SWT.NONE);
+		for(SourceDescription sd : SourceManager.getInstance().getSources()){
+			sourceCombos.add(sd.getName());
+		}
+		sourceCombos.select(0);
+		return c;
 	}
+	
+	@Override
+	protected void okPressed() {
+		// TODO Auto-generated method stub
+		sourceName = sourceCombos.getText();
+		super.okPressed();
+	}
+
+	public Combo getSourceCombos() {
+		return sourceCombos;
+	}
+
+	public void setSourceCombos(Combo sourceCombos) {
+		this.sourceCombos = sourceCombos;
+	}
+
+	public String getSourceName() {
+		return sourceName;
+	}
+
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
+	
+	
 
 }
