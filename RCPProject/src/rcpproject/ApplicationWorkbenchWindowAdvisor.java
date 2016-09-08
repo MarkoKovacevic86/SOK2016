2db0763777a895c1e21ccd3f03d15d3e6d856b7b
Igor Dojcinovic <igor@sonicline.rs>
2016-09-08 18:19:46 +0200

    Napravljen toolbar, povezan sa hendlerom i odgovarajucim prozoro koji se otvara

--- RCPProject/src/rcpproject/ApplicationWorkbenchWindowAdvisor.java
+++ RCPProject/src/rcpproject/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,8 @@
 package rcpproject;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
@@ -9,6 +11,12 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
 		super(configurer);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+		// TODO Auto-generated method stub
+		return new ApplicationActionBarAdvisor(configurer);
 	}
 	
 	public void preOpen(){
--- RCPProject/src/rcpproject/handlers/GraphHandler.java
+++ RCPProject/src/rcpproject/handlers/GraphHandler.java
@@ -3,6 +3,7 @@ package rcpproject.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -69,6 +70,23 @@ public class GraphHandler extends AbstractHandler {
 			}
 		}
 		return null;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
 	}
 	
 
