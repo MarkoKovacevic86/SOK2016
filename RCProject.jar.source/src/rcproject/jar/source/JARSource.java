e1c2af75051fc1102706343e0839bc351afec362
markokovacevic86 <markokovacevic93@hotmail.com>
2016-09-10 13:28:25 +0200

    Dovrsen rad sa source dialogom i preuzimanje izvora fajla iz prozora

--- RCProject.jar.source/src/rcproject/jar/source/JARSource.java
+++ RCProject.jar.source/src/rcproject/jar/source/JARSource.java
@@ -1,16 +1,29 @@
 package rcproject.jar.source;
 
+import java.util.jar.JarFile;
+
+import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IWorkbenchWindow;
 
+import rcpproject.jar.model.JARFile;
 import rcpproject.model.Graph;
 import rcpproject.source.ISource;
 
 
 public class JARSource implements ISource{
 
+	String location;
+	
 	@Override
 	public Graph getSource(IWorkbenchWindow window) {
 		// TODO Auto-generated method stub
+		SourceDialog dlg = new SourceDialog(window);
+		int response = dlg.open();
+		if(response == Window.OK){
+			location = dlg.getLocation();
+			JARFile file = new JARFile(location);
+			return file;
+		}
 		return null;
 	}
 
